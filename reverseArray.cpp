d928299cf67ca49d3b5049297e6643cd6f74d7aa
TJS1 <avantikasarkar18@gmail.com>
2022-03-22 20:52:30 +0530

    reverse string/array 22-03

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -1,1 +1,11 @@
-#include <bits/stdc++.h>
+string reverseWord(string str){
+    int end = str.length() - 1;
+    int start = 0;
+    while(start< end) {
+        char temp = str[start];
+        str[start] = str[end ];
+        str[end] = temp;
+        start++; end--;
+    }
+    return str;
+}
